c904121d2e2dbd8b369638f80071a053c44f6b5f
Shekhar Sharma <itsmes.sh@gmail.com>
2015-06-13 18:09:37 +0530

    Heavy updates on Layout, UI, added one more breakpoint for 1170px - responsive. Typography improved. Fancy transitions removed.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,79 +1,77 @@
-module.exports = function(grunt){
-	"use strict";
-	
-	require("matchdep").filter("grunt-*").forEach(grunt.loadNpmTasks);
-	
+module.exports = function (grunt) {
+    "use strict";
+
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
     grunt.initConfig({
-        pkg: grunt.file.readJSON('PACKAGE.json'),
-		
-		htmlhint: {
-			build: {
-				options: {
-					'tag-pair': true,
-					'tagname-lowercase': true,
-					'attr-lowercase': true,
-					'attr-value-double-quotes': true,
-					'doctype-first': true,
-					'spec-char-escape': true,
-					'id-unique': true,
-					'style-disabled': true
-				},
-				src: ['index.html', 'page.html', 'portfolio.html', 'blog.html', 'contact.html']
-			}
-		},
-		
-		uglify: {
-			build: {
-				files: {
-					"assets/js/script.min.js": ["assets/js/script.js"]
-				}
-			}
-		},
-		
-		less: {
-			development: {
-				option: {
-					paths: ["assets/css"]
-				},
-				files: {
-					"assets/css/styles.css": "assets/css/styles.less"
-				}
-			}
-		},
-		
-		cssmin: {
-			add_banner: {
-				 options: {
-				  banner: '/* It-Fits minified css file */'
-				},
-				files: {
-					"assets/css/styles.min.css": ["assets/css/normalize.css", "assets/css/styles.css"]
-				}
-			}
-	    },
-		
-		
-		watch: {
-			html: {
-				files: ['index.html', 'page.html', 'portfolio.html', 'blog.html', 'contact.html'],
-				tasks: ['htmlhint']
-			},
-			js: {
-				files: ['assets/js/script.js'],
-				tasks: ['uglify']
-			},
-			less: {
-				files: ['assets/css/styles.less'],
-				tasks: ['less']
-			},
-			css: {
-				files: ['assets/css/styles.css'],
-				tasks: ['cssmin']
-			}
-			
-		}
+
+        htmlhint: {
+            build: {
+                options: {
+                    'tag-pair': true,
+                    'tagname-lowercase': true,
+                    'attr-lowercase': true,
+                    'attr-value-double-quotes': true,
+                    'doctype-first': true,
+                    'spec-char-escape': true,
+                    'id-unique': true,
+                    'style-disabled': true
+                },
+                src: ['index.html', 'page.html', 'portfolio.html', 'blog.html', 'contact.html']
+            }
+        },
+
+        uglify: {
+            build: {
+                files: {
+                    "assets/js/script.min.js": ["assets/js/script.js"]
+                }
+            }
+        },
+
+        less: {
+            development: {
+                options: {
+                    paths: ["assets/css"],
+                    sourceMap: true,
+                    sourceMapFilename: "assets/css/styles.css.map",
+                    sourceMapURL: "assets/css/styles.css.map"
+                },
+                files: {
+                    "assets/css/styles.css": "assets/css/styles.less"
+                }
+            }
+        },
+
+        cssmin: {
+            add_banner: {
+                options: {
+                    banner: '/* It-Fits minified css file */',
+                    processImport: false
+                },
+                files: {
+                    "assets/css/styles.min.css": ["assets/css/styles.css"]
+                }
+            }
+        },
+
+
+        watch: {
+            html: {
+                files: ['index.html', 'page.html', 'portfolio.html', 'blog.html', 'contact.html'],
+                tasks: ['htmlhint']
+            },
+            js: {
+                files: ['assets/js/script.js'],
+                tasks: ['uglify']
+            },
+            less: {
+                files: ['assets/css/*.less'],
+                tasks: ['less', "cssmin"]
+            }
+        }
     });
 
-    grunt.registerTask('default', []);
+    grunt.registerTask('default', ["watch:less"]);
 
 };
